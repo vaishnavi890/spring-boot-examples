9aec323443ff378c33c4b3f3f20ce75bbe917e8c
Vaishnavi Chevale <130659974+vaishnavi890@users.noreply.github.com>
2025-04-23 17:28:15 +0530

    initial import

---
+++ spring-boot-logging/src/main/java/com/vaishnavi/practice/springboot/controller/GreetingsController.java
@@ -0,0 +1,26 @@
+package com.vaishnavi.practice.springboot.controller;
+
+import com.vaishnavi.practice.springboot.services.GreetingsService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingsController {
+
+    private Logger logger = LoggerFactory.getLogger(GreetingsController.class);
+
+    @Autowired(required = true)
+    private GreetingsService greetingsService;
+
+    @GetMapping("/hello")
+    public String controlGreetings(@RequestParam(name = "name", required = false) String name) {
+        logger.trace("i came to /hello controller");
+        return greetingsService.sayHello(name);
+    }
+}
+
+
