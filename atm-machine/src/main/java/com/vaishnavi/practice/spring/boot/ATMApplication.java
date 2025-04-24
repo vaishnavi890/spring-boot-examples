e860696f15b14b6fc4532f6738311c38fca907c0
Vaishnavi Chevale <130659974+vaishnavi890@users.noreply.github.com>
2025-04-24 17:08:11 +0530

    initial import

---
+++ atm-machine/src/main/java/com/vaishnavi/practice/spring/boot/ATMApplication.java
@@ -0,0 +1,16 @@
+package com.vaishnavi.practice.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+public class ATMApplication
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(ATMApplication.class, args);
+}
+}
