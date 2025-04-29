01b24ef17df33410fe4d03695a9476746fdb5567
Vaishnavi Chevale <130659974+vaishnavi890@users.noreply.github.com>
2025-04-29 18:04:54 +0530

    initial import

--- store-management-system/src/main/java/com/vaishnavi/practice/spring/boot/model/Product.java
+++ store-management-system/src/main/java/com/vaishnavi/practice/spring/boot/model/Product.java
@@ -8,6 +8,23 @@ public class Product {
     String name;
     double price;
     int quantity;
+
+    public Integer getId() {
+        return 0;
+    }
+
+    public String getName() {
+        return "";
+    }
+
+    public void setName(String name) {
+    }
+
+    public void setPrice(double price) {
+    }
+
+    public void setQuantity(int quantity) {
+    }
 }
 
 
