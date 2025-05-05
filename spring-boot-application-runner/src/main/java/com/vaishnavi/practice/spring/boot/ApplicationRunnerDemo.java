bca7219ddc1bab1e160edea5c31f05f18206aa75
Vaishnavi Chevale <130659974+vaishnavi890@users.noreply.github.com>
2025-05-05 12:30:16 +0530

    initial import

---
+++ spring-boot-application-runner/src/main/java/com/vaishnavi/practice/spring/boot/ApplicationRunnerDemo.java
@@ -0,0 +1,26 @@
+package com.vaishnavi.practice.spring.boot;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationRunnerDemo implements ApplicationRunner {
+
+    private Logger logger = LoggerFactory.getLogger(ApplicationRunnerDemo.class);
+
+    @Override
+    public void run(final ApplicationArguments args) throws Exception {
+        logger.info("Total NonOptionArgs: {}", args.getNonOptionArgs().size());
+        args.getNonOptionArgs().forEach( logger::info);
+        logger.info("Total OptionArgs: {}", args.getOptionNames().size());
+        args.getOptionNames().forEach(name -> logger.info("{} = {}",name, args.getOptionValues(name)));
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationRunnerDemo.class, args);
+    }
+}
