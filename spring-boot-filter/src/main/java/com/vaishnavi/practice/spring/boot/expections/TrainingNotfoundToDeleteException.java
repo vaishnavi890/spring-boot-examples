6ae1ab2fd53ac2a6b38a8666fd72483baf90b348
Vaishnavi Chevale <130659974+vaishnavi890@users.noreply.github.com>
2025-05-08 17:13:20 +0530

    initial import

---
+++ spring-boot-filter/src/main/java/com/vaishnavi/practice/spring/boot/expections/TrainingNotfoundToDeleteException.java
@@ -0,0 +1,6 @@
+package com.vaishnavi.practice.spring.boot.expections;
+
+public class TrainingNotfoundToDeleteException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+}
+
