1529e059a01145f093eb8c14c1e9e5807c35fc61
Vaishnavi Chevale <130659974+vaishnavi890@users.noreply.github.com>
2025-05-13 15:57:39 +0530

    initial import

---
+++ spring-boot-internationalization/src/main/java/com/vaishnavi/practice/spring/boot/InternationalizationDemoApplication.java
@@ -0,0 +1,17 @@
+package com.vaishnavi.practice.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class InternationalizationDemoApplication
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(InternationalizationDemoApplication.class, args);
+    }
+}
