d92cf3c26f61195ad79589b5699546df0c34bf2c
Vaishnavi Chevale <130659974+vaishnavi890@users.noreply.github.com>
2025-05-14 16:43:47 +0530

    initial import

---
+++ eureka-server-discovery-service/spring-eureka-client-student-service/src/main/java/com/vaishnavi/practice/spring/boot/SpringEurekaClientStudentServiceApplication.java
@@ -0,0 +1,17 @@
+package com.vaishnavi.practice.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * Hello world!
+ *
+ */
+public class SpringEurekaClientStudentServiceApplication
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(SpringEurekaClientStudentServiceApplication.class, args);
+    }
+}
