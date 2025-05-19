8b447e41d61ab81431e2424bf9ce76e715c0869a
Vaishnavi Chevale <130659974+vaishnavi890@users.noreply.github.com>
2025-05-19 20:53:12 +0530

    initial import

---
+++ spring-boot-interceptor/src/main/java/com/vaishnavi/practice/spring/boot/model/Training.java
@@ -0,0 +1,23 @@
+package com.vaishnavi.practice.spring.boot.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Training {
+
+    private int id;
+    private String name;
+    private int duration;
+    private String prerequisite;
+
+    public Training(String name, int duration, String prerequisite) {
+        this.name = name;
+        this.duration = duration;
+        this.prerequisite = prerequisite;
+    }
+}
+
