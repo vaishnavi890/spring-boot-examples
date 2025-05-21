0aa6f085bf92c3c001d09971392138d6cf2b0029
Vaishnavi Chevale <130659974+vaishnavi890@users.noreply.github.com>
2025-05-21 18:01:55 +0530

    initial import

---
+++ spring-boot-rest-service/src/main/java/com/vaishnavi/practice/spring/boot/RestfulBootApplication.java
@@ -0,0 +1,13 @@
+package com.vaishnavi.practice.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+public class RestfulBootApplication
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(RestfulBootApplication.class, args);
+    }
+}
