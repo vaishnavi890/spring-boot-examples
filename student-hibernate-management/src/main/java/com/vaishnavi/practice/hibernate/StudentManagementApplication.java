5a6b75032c45e42190d08fcea83667734c2885eb
Vaishnavi Chevale <130659974+vaishnavi890@users.noreply.github.com>
2025-05-25 22:23:59 +0530

    initial import

---
+++ student-hibernate-management/src/main/java/com/vaishnavi/practice/hibernate/StudentManagementApplication.java
@@ -0,0 +1,12 @@
+package com.vaishnavi.practice.hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class StudentManagementApplication
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(StudentManagementApplication.class, args);
+    }
+}
