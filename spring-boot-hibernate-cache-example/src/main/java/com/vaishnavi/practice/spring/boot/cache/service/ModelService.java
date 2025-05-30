ee01141d5755d7b7798da5090200d71727ebbb73
Vaishnavi Chevale <130659974+vaishnavi890@users.noreply.github.com>
2025-05-30 15:52:04 +0530

    initial import

---
+++ spring-boot-hibernate-cache-example/src/main/java/com/vaishnavi/practice/spring/boot/cache/service/ModelService.java
@@ -0,0 +1,16 @@
+package com.vaishnavi.practice.spring.boot.cache.service;
+
+import java.util.List;
+import java.util.Set;
+
+public interface ModelService<M,D> {
+
+    M save(M m);
+    boolean delete(final Long id);
+    List<M> getAllRecords();
+    M getRecordById(final Long id);
+    Set<M> getRecordByAttribute(String attribute);
+    M toModel(D d);
+    D toEntity(M m);
+}
+
