5c3e1565e41f4731f72b3b46ba3ac644c9bc0907
Vaishnavi Chevale <130659974+vaishnavi890@users.noreply.github.com>
2025-05-30 15:45:18 +0530

    initial import

---
+++ spring-boot-hibernate-cache-example/src/main/java/com/vaishnavi/practice/spring/boot/cache/repository/EntityRepository.java
@@ -0,0 +1,20 @@
+package com.vaishnavi.practice.spring.boot.cache.repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+public interface EntityRepository<T> {
+    T save(T object);
+
+    List<T> findAll();
+
+    Optional<T> findById(Long id);
+
+    Optional<T> findByName(String name);
+
+    Set<T> findByAttribute(String value);
+
+    void deleteById(Long id);
+}
+
