6169400160b98eda7ef6090eac4cc8a5a6ea1256
Vaishnavi Chevale <130659974+vaishnavi890@users.noreply.github.com>
2025-05-30 16:02:24 +0530

    initial import

---
+++ spring-boot-hibernate-cache-example/src/main/java/com/vaishnavi/practice/spring/boot/cache/SpringBootHibernateApplication.java
@@ -0,0 +1,12 @@
+package com.vaishnavi.practice.spring.boot.cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class SpringBootHibernateApplication
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(SpringBootHibernateApplication.class, args);
+    }
+}
