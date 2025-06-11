ca559a382fece715bf3c37988b3b1cde0b42dea5
Vaishnavi Chevale <130659974+vaishnavi890@users.noreply.github.com>
2025-06-11 22:08:36 +0530

    advance concept

---
+++ student-hibernate-management1/src/main/java/com/vaishnavi/spring/boot/hibernate/controller/StudentController.java
@@ -0,0 +1,51 @@
+package com.vaishnavi.spring.boot.hibernate.controller;
+
+import com.vaishnavi.spring.boot.hibernate.model.Student;
+import com.vaishnavi.spring.boot.hibernate.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Set;
+
+@RestController
+@RequestMapping("/api/studentManagement")
+public class StudentController {
+
+    @Autowired
+    private StudentService service;
+
+    @PostMapping("/student")
+    public ResponseEntity<Student> createStudent(@RequestBody Student student) {
+        System.out.println("Inside controller: " + student);
+        return new ResponseEntity(service.createStudent(student), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/student/{id}")
+    public ResponseEntity<Student> createStudent(@PathVariable("id") int studentId) {
+        System.out.println("Inside controller: " + studentId);
+        return new ResponseEntity(service.deleteStudent(studentId), HttpStatus.OK);
+    }
+
+    @GetMapping("/student/{id}")
+    public ResponseEntity<Student> retrieveStudent(@PathVariable("id") int studentId) {
+        System.out.println("Inside controller: " + studentId);
+        return new ResponseEntity(service.retrieveStudent(studentId), HttpStatus.OK);
+    }
+
+    @GetMapping("/students")
+    public List<Student> retrieveStudent() {
+        return service.retrieveStudents();
+    }
+
+    @GetMapping("/students/pagination")
+    public ResponseEntity<List<Student>> retrieveStudent(
+            @RequestParam(name = "page", defaultValue = "0") int page,
+            @RequestParam(name = "size", defaultValue = "10") int size) {
+        System.out.println(page + "|" + size);
+        return new ResponseEntity<>(service.findStudentWithPagination(page, size), HttpStatus.OK);
+    }
+}
+
