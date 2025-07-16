01694f4e534e71c4d654e40775a5f46ff12fbd1d
Vaishnavi Chevale <130659974+vaishnavi890@users.noreply.github.com>
2025-07-16 16:01:52 +0530

    initial import

---
+++ spring-boot-security/src/main/java/com/vaishnavi/practice/spring/boot/security/token/generator/process/model/JwtResponse.java
@@ -0,0 +1,26 @@
+package com.vaishnavi.practice.spring.boot.security.token.generator.process.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.io.Serializable;
+
+public class JwtResponse implements Serializable {
+
+    private static final long serialVersionUID = -8091879091924046844L;
+
+    @JsonProperty("token")
+    private final String jwttoken;
+
+    public JwtResponse(String jwttoken) {
+        this.jwttoken = jwttoken;
+    }
+
+    public JwtResponse() {
+        this.jwttoken = "";
+    }
+
+    public String getToken() {
+        return this.jwttoken;
+    }
+}
+
