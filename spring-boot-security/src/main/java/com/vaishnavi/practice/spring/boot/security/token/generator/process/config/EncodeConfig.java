11210ce3b1e41ff04176bf65a0088c424416d167
Vaishnavi Chevale <130659974+vaishnavi890@users.noreply.github.com>
2025-07-16 15:52:22 +0530

    initial import

---
+++ spring-boot-security/src/main/java/com/vaishnavi/practice/spring/boot/security/token/generator/process/config/EncodeConfig.java
@@ -0,0 +1,16 @@
+package com.vaishnavi.practice.spring.boot.security.token.generator.process.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class EncodeConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
+
