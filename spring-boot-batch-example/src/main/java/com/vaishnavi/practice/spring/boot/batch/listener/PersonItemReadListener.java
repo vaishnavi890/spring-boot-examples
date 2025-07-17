c1bf710365198ca50ecbd7da13b4039a8676197e
Vaishnavi Chevale <130659974+vaishnavi890@users.noreply.github.com>
2025-07-17 22:40:40 +0530

    initial import

---
+++ spring-boot-batch-example/src/main/java/com/vaishnavi/practice/spring/boot/batch/listener/PersonItemReadListener.java
@@ -0,0 +1,25 @@
+package com.vaishnavi.practice.spring.boot.batch.listener;
+
+import com.vaishnavi.practice.spring.boot.batch.jobs.csvToDb.model.Person;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.ItemReadListener;
+
+@Slf4j
+public class PersonItemReadListener implements ItemReadListener<Person> {
+
+    @Override
+    public void beforeRead() {
+        log.info("Reading a new Person Record");
+    }
+
+    @Override
+    public void afterRead(Person input) {
+        log.info("New Person record read : " + input);
+    }
+
+    @Override
+    public void onReadError(Exception e) {
+        log.error("Error in reading the person record : " + e);
+    }
+}
+
