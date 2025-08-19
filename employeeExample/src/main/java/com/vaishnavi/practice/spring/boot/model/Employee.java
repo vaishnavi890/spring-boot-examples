e2ec11fb43f802c75580e3313c0fb4c1fb367e41
Vaishnavi Chevale <130659974+vaishnavi890@users.noreply.github.com>
2025-08-19 11:52:09 +0530

    initial import

---
+++ employeeExample/src/main/java/com/vaishnavi/practice/spring/boot/model/Employee.java
@@ -0,0 +1,38 @@
+package com.vaishnavi.practice.spring.boot.model;
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+
+import jakarta.persistence.*;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+@Entity
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String role;
+
+    // Constructors
+    public Employee() {}
+    public Employee(String name, String role) {
+        this.name = name;
+        this.role = role;
+    }
+
+    // Getters & Setters
+  //  public Long getId() { return id; }
+  //  public void setId(Long id) { this.id = id; }
+
+   // public String getName() { return name; }
+  //  public void setName(String name) { this.name = name; }
+
+  //  public String getRole() { return role; }
+  //  public void setRole(String role) { this.role = role; }
+}
+
